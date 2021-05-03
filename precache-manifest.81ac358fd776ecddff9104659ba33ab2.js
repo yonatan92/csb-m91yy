3c796aef5b02bc532b5beacb7c48e151843cb3b1
Code Sandbox <hello@codesandbox.io>
2021-05-03 11:36:07 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.81ac358fd776ecddff9104659ba33ab2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b45de98d1cdbf4b433752f7edb36474",
+    "url": "https://yonatan92.github.io/csb-m91yy/index.html"
+  },
+  {
+    "revision": "fe076117d13f848b40df",
+    "url": "https://yonatan92.github.io/csb-m91yy/static/js/2.54796d9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://yonatan92.github.io/csb-m91yy/static/js/2.54796d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e0324c9de5c1629c08",
+    "url": "https://yonatan92.github.io/csb-m91yy/static/js/main.0c911a23.chunk.js"
+  },
+  {
+    "revision": "35940fe256276087910e",
+    "url": "https://yonatan92.github.io/csb-m91yy/static/js/runtime-main.357259ee.js"
+  }
+]);
